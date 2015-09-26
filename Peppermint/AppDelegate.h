acc13917638f3c47341d7b0335e2cca7ec23ad97
Okan Kurtulus <okankurtulus@gmail.com>
2015-09-26 11:06:37 +0300

    Initial Commit

---
+++ Peppermint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Peppermint
+//
+//  Created by Okan Kurtulus on 26/09/15.
+//  Copyright (c) 2015 Okan Kurtulus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Peppermint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Peppermint
+//
+//  Created by Okan Kurtulus on 26/09/15.
+//  Copyright (c) 2015 Okan Kurtulus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
