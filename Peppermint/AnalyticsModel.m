b35e6151593c6971486bb23e2dba453c956862d8
Okan Kurtulus <okankurtulus@gmail.com>
2016-05-02 10:09:35 +0200

    - Added trimming for long messages sending to Fabric

--- Peppermint/AnalyticsModel.m
+++ Peppermint/AnalyticsModel.m
@@ -12,6 +12,8 @@
 #import "GAI.h"
 #import "GAIDictionaryBuilder.h"
 
+#define EVENT_ATTR_MAX_LENGTH_FOR_CRASHLYTICS  100
+
 @implementation AnalyticsModel
 
 +(void) logError:(NSError*) error {
@@ -20,12 +22,37 @@
     [self logErrorToFlurry:error];
 }
 
++(NSMutableDictionary*) truncateEventValuesOf:(NSMutableDictionary*)logDictionary toLimit:(int) limit {
+    for(NSString *key in logDictionary.allKeys) {
+        NSString *value = nil;
+        id object =[logDictionary valueForKey:key];
+        
+        [logDictionary removeObjectForKey:key];
+        if([object isKindOfClass:[NSError class]]) {
+            value = ((NSError*)object).localizedDescription;
+        } else if ([object isKindOfClass:[NSString class]]) {
+            value = (NSString*)object;
+        }
+        
+        if(value && value.length > 0) {
+            if(value.length > limit) {
+                //Truncating values may cause loss of even data, but Crashlytics prevents length of 100. Check logs from other tracking platforms
+                // Google Analytics or Flurry
+                value = [value substringWithRange:NSMakeRange(0, limit)];
+            }
+            [logDictionary setObject:value forKey:key];
+        }
+    }
+    return logDictionary;
+}
+
 +(void) logErrorToAnswers:(NSError*) error {
     NSMutableDictionary *logDictionary = [NSMutableDictionary dictionaryWithDictionary:error.userInfo];
     [logDictionary setValue:[NSNumber numberWithInteger:error.code] forKey:@"Code"];
     [logDictionary setValue:error.domain forKey:@"Domain"];
     [logDictionary setValue:error.localizedDescription forKey:@"LocalizedDescription"];
     [logDictionary setValue:error.description forKey:@"Description"];
+    logDictionary = [self truncateEventValuesOf:logDictionary toLimit:EVENT_ATTR_MAX_LENGTH_FOR_CRASHLYTICS];
     [Answers logCustomEventWithName:@"NSError handled with alert" customAttributes:logDictionary];
 }
 
