c0b5888afd16cb4b40600727c84168e975efee4c
Okan Kurtulus <okankurtulus@gmail.com>
2016-05-12 10:49:54 +0200

    - Added AddEmailForSMSContact View

--- Peppermint/AddEmailForSMSContactView.h
+++ Peppermint/AddEmailForSMSContactView.h
@@ -14,13 +14,11 @@
 @end
 
 @interface AddEmailForSMSContactView : BaseCustomView
+@property (weak, nonatomic) IBOutlet UIView *backgroundView;
 @property (weak, nonatomic) IBOutlet UIView *contentView;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
-@property (weak, nonatomic) IBOutlet UILabel *sendViaEmailLabel;
-@property (weak, nonatomic) IBOutlet UIView         *dontAskAgainView;
-@property (weak, nonatomic) IBOutlet UIImageView    *dontAskAgainImageView;
-@property (weak, nonatomic) IBOutlet UILabel        *dontAskAgainLabel;
-@property (weak, nonatomic) IBOutlet UIButton *noButton;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UIImageView *emailValidImageView;
 @property (weak, nonatomic) IBOutlet UIButton *yesButton;
 
 @property (weak, nonatomic) id<AddEmailForSMSContactViewDelegate> delegate;
--- Peppermint/AddEmailForSMSContactView.m
+++ Peppermint/AddEmailForSMSContactView.m
@@ -9,7 +9,10 @@
 #import "AddEmailForSMSContactView.h"
 #import "CustomContactModel.h"
 
-@interface AddEmailForSMSContactView() <CustomContactModelDelegate>
+#define SIZE_BIG        17
+#define SIZE_MIDDLE     15
+
+@interface AddEmailForSMSContactView() <CustomContactModelDelegate, UITextFieldDelegate>
 @end
 
 @implementation AddEmailForSMSContactView {
@@ -19,9 +22,82 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
+    self.hidden = YES;
     customContactModel = [CustomContactModel new];
     customContactModel.delegate = self;
     peppermintContactToModify = nil;
+    
+    self.contentView.layer.cornerRadius = 15;
+    self.contentView.backgroundColor = [UIColor peppermintGray248];
+
+    [self initTitleText];
+    UIGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]
+                                       initWithTarget:self action:@selector(hide)];
+    [self.backgroundView addGestureRecognizer:tapGesture];
+    
+    self.emailTextField.delegate = self;
+    [self.emailTextField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+}
+
+-(void) initTitleText {
+    NSMutableAttributedString *titleText = [NSMutableAttributedString new];
+    [titleText addText:LOC(@"Sending via Email", @"title")
+                ofSize:SIZE_BIG
+               ofColor:[UIColor blackColor]
+               andFont:[UIFont openSansBoldFontOfSize:SIZE_BIG]];
+    
+    [titleText addText:@"\n\n" ofSize:SIZE_BIG ofColor:[UIColor clearColor]];
+    
+    [titleText addText:LOC(@"No email address found. Please insert an email address for this contact.", @"information")
+                ofSize:SIZE_MIDDLE
+               ofColor:[UIColor blackColor]
+               andFont:[UIFont openSansSemiBoldFontOfSize:SIZE_MIDDLE]];
+    
+    [titleText centerText];
+    
+    self.titleLabel.attributedText = titleText;
+}
+
+-(void) yesButtonActive {
+    self.yesButton.backgroundColor      = [UIColor clearColor];
+    self.yesButton.layer.borderColor    = [UIColor blackColor].CGColor;
+    self.yesButton.tintColor = [UIColor warningColor];
+    [self.yesButton setTitle:LOC(@"Save email address", @"Title") forState:UIControlStateNormal];
+}
+
+-(void) yesButtonInActive {
+    self.yesButton.backgroundColor      = [UIColor clearColor];
+    self.yesButton.layer.borderColor    = [UIColor blackColor].CGColor;
+    self.yesButton.tintColor = [UIColor peppermintCancelOrange];
+    [self.yesButton setTitle:@"Cancel" forState:UIControlStateNormal];
+}
+
+-(void)textFieldDidChange :(UITextField *)textField {
+    textField.text = textField.text.normalizeText;
+    [self updateEmailValidImageView];
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    BOOL result = NO;
+    if([string isEqualToString:DONE_STRING]) {
+        [self yesButtonPressed:self.yesButton];
+    } else {
+        result = YES;
+    }
+    return result;
+}
+
+-(void) updateEmailValidImageView {
+    BOOL isEmailValid = [self.emailTextField.text isValidEmail];
+    if(isEmailValid) {
+        self.emailValidImageView.image = [UIImage imageNamed:@"email_active"];
+        [self yesButtonActive];
+        [self.emailTextField updateKeyboardReturnType:UIReturnKeyDone];
+    } else {
+        self.emailValidImageView.image = [UIImage imageNamed:@"email_inactive"];
+        [self yesButtonInActive];
+        [self.emailTextField updateKeyboardReturnType:UIReturnKeyDefault];
+    }
 }
 
 +(instancetype) createInstanceWithDelegate:(id<AddEmailForSMSContactViewDelegate>) delegate {
@@ -35,7 +111,10 @@
 
 -(void) presentOverView:(UIView*)view forPeppermintContact:(PeppermintContact*)peppermintContact {
     peppermintContactToModify = peppermintContact;
+    self.emailTextField.text = @"";
     [self showInView:view];
+    [self updateEmailValidImageView];
+    [self.emailTextField becomeFirstResponder];
 }
 
 -(void) showInView:(UIView*) view {
@@ -50,33 +129,28 @@
 }
 
 -(void) hide {
+    [self.emailTextField resignFirstResponder];
     self.hidden = YES;
 }
 
 #pragma mark - Button Actions
 
--(IBAction)confirm:(id)sender {
+-(IBAction)yesButtonPressed:(id)sender {
     NSLog(@"confirm:");
+    NSString *email = self.emailTextField.text;
+    [self performAddEmailOperationsWithEmail:email];
     [self hide];
 }
 
--(IBAction)decline:(id)sender {
-    NSLog(@"decline:");
-    [self hide];
-}
-
--(IBAction)dontAskAgainButtonPressed:(id)sender {
-    NSLog(@"dontAskAgainButtonPressed:");
-}
-
--(IBAction) sendViaEmailButtonPressed {
-    NSLog(@"sendViaEmailButtonPressed");
-    
-    peppermintContactToModify.communicationChannel = CommunicationChannelEmail;
-    peppermintContactToModify.communicationChannelAddress = [NSString stringWithFormat:@"%@@yopmail.com", peppermintContactToModify.nameSurname];
-    [MBProgressHUD showHUDAddedTo:self.contentView animated:YES];
-    [customContactModel save:peppermintContactToModify];
-    
+-(void) performAddEmailOperationsWithEmail:(NSString*) email {
+    if(!email.isValidEmail) {
+        NSLog(@"Email:%@ is not valid", email);
+    } else {
+        peppermintContactToModify.communicationChannel = CommunicationChannelEmail;
+        peppermintContactToModify.communicationChannelAddress = email;
+        [MBProgressHUD showHUDAddedTo:self.contentView animated:YES];
+        [customContactModel save:peppermintContactToModify];
+    }
 }
 
 #pragma mark - CustomContactModelDelegate
